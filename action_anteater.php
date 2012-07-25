1082776b094e39c23bc2142944a01fdbdfb1ebcf
Kazutaka Miyasaka <kazmiya@gmail.com>
2012-07-26 03:48:25 +0900

    Fix to follow my coding standards

--- action_anteater.php
+++ action_anteater.php
@@ -7,13 +7,19 @@
  */
 
 // must be run within DokuWiki
-if (!defined('DOKU_INC')) die();
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
+if (!defined('DOKU_INC')) {
+    die();
+}
 
-require_once(DOKU_PLUGIN.'action.php');
-require_once(DOKU_PLUGIN.'preservefilenames/common.php');
+if (!defined('DOKU_PLUGIN')) {
+    define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
+}
 
-class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
+require_once(DOKU_PLUGIN . 'action.php');
+require_once(DOKU_PLUGIN . 'preservefilenames/common.php');
+
+class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin
+{
     /**
      * Common functions
      */
@@ -22,7 +28,8 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
     /**
      * Registers event handlers
      */
-    function register(&$controller) {
+    function register(&$controller)
+    {
         $this->common = new PreserveFilenames_Common();
 
         $controller->register_hook('MEDIA_UPLOAD_FINISH',          'AFTER',  $this, '_saveMeta');
@@ -39,29 +46,37 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
     /**
      * Saves the name of the uploaded media file to a meta file
      */
-    function _saveMeta(&$event) {
+    function _saveMeta(&$event)
+    {
         global $conf;
 
         $id = $event->data[2];
         $filename_tidy = noNS($id);
 
         // retrieve original filename
-        if (!empty($_POST['id'])) { // via normal uploader
+        if (!empty($_POST['id'])) {
+            // via normal uploader
             $filename_pat = $conf['useslash'] ? '/([^:;\/]*)$/' : '/([^:;]*)$/';
             preg_match($filename_pat, $_POST['id'], $matches);
             $filename_orig = $matches[1];
-        } elseif (isset($_FILES['Filedata'])) { // via multiuploader
+        } elseif (isset($_FILES['Filedata'])) {
+            // via multiuploader
             $filename_orig = $_FILES['upload']['name'];
         } else {
             return;
         }
+
         $filename_safe = $this->common->_sanitizeFileName($filename_orig);
 
         // no need to save original filename
-        if ($filename_tidy === $filename_safe) return;
+        if ($filename_tidy === $filename_safe) {
+            return;
+        }
 
         // fallback if suspicious characters found
-        if ($filename_orig !== $filename_safe) return;
+        if ($filename_orig !== $filename_safe) {
+            return;
+        }
 
         // save original filename to metadata
         $metafile = metaFN($id, '.filename');
@@ -73,10 +88,14 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
     /**
      * Deletes a meta file associated with the deleted media file
      */
-    function _deleteMeta(&$event) {
+    function _deleteMeta(&$event)
+    {
         $id = $event->data['id'];
         $metafile = metaFN($id, '.filename');
-        if (@unlink($metafile)) io_sweepNS($id, 'metadir');
+
+        if (@unlink($metafile)) {
+            io_sweepNS($id, 'metadir');
+        }
     }
 
     /**
@@ -84,7 +103,8 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
      * 
      * @see sendFile() in lib/exe/fetch.php
      */
-    function _sendFile(&$event) {
+    function _sendFile(&$event)
+    {
         global $conf;
         global $MEDIA;
 
@@ -96,37 +116,46 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
 
         // send headers
         header("Content-Type: $mime");
+
         // smart http caching headers
         if ($cache == -1) {
             // cache
             // cachetime or one hour
-            header('Expires: '.gmdate('D, d M Y H:i:s', time() + max($conf['cachetime'], 3600)).' GMT');
-            header('Cache-Control: public, proxy-revalidate, no-transform, max-age='.max($conf['cachetime'], 3600));
+            header('Expires: ' . gmdate('D, d M Y H:i:s', time() + max($conf['cachetime'], 3600)) . ' GMT');
+            header('Cache-Control: public, proxy-revalidate, no-transform, max-age=' . max($conf['cachetime'], 3600));
             header('Pragma: public');
         } elseif ($cache > 0) {
             // recache
             // remaining cachetime + 10 seconds so the newly recached media is used
-            header('Expires: '.gmdate("D, d M Y H:i:s", $fmtime + $conf['cachetime'] + 10).' GMT');
-            header('Cache-Control: public, proxy-revalidate, no-transform, max-age='.max($fmtime - time() + $conf['cachetime'] + 10, 0));
+            header('Expires: ' . gmdate("D, d M Y H:i:s", $fmtime + $conf['cachetime'] + 10) . ' GMT');
+            header('Cache-Control: public, proxy-revalidate, no-transform, max-age=' . max($fmtime - time() + $conf['cachetime'] + 10, 0));
             header('Pragma: public');
         } elseif ($cache == 0) {
             // nocache
             header('Cache-Control: must-revalidate, no-transform, post-check=0, pre-check=0');
             header('Pragma: public');
         }
+
         // send important headers first, script stops here if '304 Not Modified' response
         http_conditionalRequest($fmtime);
 
         // retrieve original filename and send Content-Disposition header
         $filename = $this->_getOriginalFileName($MEDIA);
-        if ($filename === false) $filename = utf8_decodeFN($this->common->_correctBasename($d['file']));
+
+        if ($filename === false) {
+            $filename = utf8_decodeFN($this->common->_correctBasename($d['file']));
+        }
+
         header($this->common->_buildContentDispositionHeader($dl, $filename));
 
         // use x-sendfile header to pass the delivery to compatible webservers
-        if (http_sendfile($file)) exit;
+        if (http_sendfile($file)) {
+            exit;
+        }
 
         // send file contents
         $fp = @fopen($file, 'rb');
+
         if ($fp) {
             http_rangeRequest($fp, filesize($file), $mime);
         } else {
@@ -138,11 +167,12 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
     /**
      * Replaces titles of non-labeled internal media links with their original filenames
      */
-    function _replaceLinkTitle(&$event) {
+    function _replaceLinkTitle(&$event)
+    {
         global $ID;
         global $conf;
 
-        require_once(DOKU_INC.'inc/JpegMeta.php');
+        require_once(DOKU_INC . 'inc/JpegMeta.php');
 
         $ns = getNS($ID);
 
@@ -154,11 +184,14 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
 
         // scan media link and mark it with its original filename
         $last = count($calls) - 1;
+
         for ($i = 0; $i <= $last; $i++) {
             // NOTE: 'externalmedia' is processed here because there is a
             //       basename() bug in fetching its auto-filled linktext.
             //       For more details please see common->_correctBasename().
-            if (!preg_match('/^(?:in|ex)ternalmedia$/', $calls[$i][$handler_name])) continue;
+            if (!preg_match('/^(?:in|ex)ternalmedia$/', $calls[$i][$handler_name])) {
+                continue;
+            }
 
             $inst =& $calls[$i][$instructions];
             $filename = false;
@@ -177,10 +210,12 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
 
             // prefetch auto-filled linktext
             if (!$linktext) {
-                if (substr($mime, 0, 5) === 'image'
-                        && ($ext === 'jpg' || $ext === 'jpeg')
-                        && ($jpeg = new JpegMeta(mediaFN($src)))
-                        && ($caption = $jpeg->getTitle())) {
+                if (
+                    substr($mime, 0, 5) === 'image'
+                    && ($ext === 'jpg' || $ext === 'jpeg')
+                    && ($jpeg = new JpegMeta(mediaFN($src)))
+                    && ($caption = $jpeg->getTitle())
+                ) {
                     $linktext = $caption;
                 } else {
                     $linktext = $this->common->_correctBasename(noNS($src));
@@ -192,9 +227,9 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
             if ($filename === false) {
                 $inst[$title] = $linktext;
             } elseif ($inst[$title] !== $linktext) {
-                $inst[$title] = $linktext.'}}preservefilenames:autofilled:'.$filename;
+                $inst[$title] = $linktext . '}}preservefilenames:autofilled:' . $filename;
             } else {
-                $inst[$title] = $linktext.'}}preservefilenames::'.$filename;
+                $inst[$title] = $linktext . '}}preservefilenames::' . $filename;
             }
         }
     }
@@ -202,8 +237,11 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
     /**
      * Replaces url and title of a link which has original filename info
      */
-    function _replaceLinkURL(&$event) {
-        if ($event->data[0] !== 'xhtml') return;
+    function _replaceLinkURL(&$event)
+    {
+        if ($event->data[0] !== 'xhtml') {
+            return;
+        }
 
         // image link
         $event->data[1] = preg_replace_callback(
@@ -231,9 +269,13 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
     /**
      * Callback function for _replaceLinkURL (link)
      */
-    static function _replaceLinkURL_callback_a($matches) {
+    static function _replaceLinkURL_callback_a($matches)
+    {
         list($atag, $attr_str, $linktext, $autofilled, $filename) = array_slice($matches, 0, 5);
-        if (!preg_match('/class="media[" ]/', $attr_str)) return $atag;
+
+        if (!preg_match('/class="media[" ]/', $attr_str)) {
+            return $atag;
+        }
 
         if (isset($matches[5])) {
             // image link
@@ -241,7 +283,9 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
             $linktext = self::_replaceLinkURL_callback_img(array_slice($matches, 5, 4));
         } else {
             // text link
-            if ($autofilled) $linktext = $filename;
+            if ($autofilled) {
+                $linktext = $filename;
+            }
         }
 
         $filename = htmlspecialchars_decode($filename, ENT_QUOTES);
@@ -249,39 +293,45 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
 
         $attr_str = preg_replace(
             array(
-                '/(href="[^"]*)'.preg_quote(rawurlencode($pageid)).'((?:\?[^#]*)?(?:#[^"]*)?")/',
-                '/(title="[^"]*)'.preg_quote($pageid).'(")/'
+                '/(href="[^"]*)' . preg_quote(rawurlencode($pageid)) . '((?:\?[^#]*)?(?:#[^"]*)?")/',
+                '/(title="[^"]*)' . preg_quote($pageid) . '(")/'
             ),
             array(
-                "\${1}".rawurlencode($filename).'\2',
-                "\${1}".hsc($filename).'\2'
+                "\${1}" . rawurlencode($filename) . '\2',
+                "\${1}" . hsc($filename) . '\2'
             ),
             $attr_str
         );
 
-        return "<a ".$attr_str.">".$linktext."</a>";
+        return '<a ' . $attr_str . '>' . $linktext . '</a>';
     }
 
     /**
      * Callback function for _replaceLinkURL (image)
      */
-    static function _replaceLinkURL_callback_img($matches) {
+    static function _replaceLinkURL_callback_img($matches)
+    {
         list($imgtag, $imgtitle, $autofilled, $filename) = $matches;
-        if (!preg_match('/class="media(?:left|right|center)?[" ]/', $imgtag)) return $imgtag;
 
-        if ($autofilled) $imgtitle = $filename;
+        if (!preg_match('/class="media(?:left|right|center)?[" ]/', $imgtag)) {
+            return $imgtag;
+        }
+
+        if ($autofilled) {
+            $imgtitle = $filename;
+        }
 
         $filename = htmlspecialchars_decode($filename, ENT_QUOTES);
         $pageid = cleanID($filename);
 
         $imgtag = preg_replace(
             array(
-                '/(src="[^"]*)'.preg_quote(rawurlencode($pageid)).'((?:\?[^#]*)?(?:#[^"]*)?")/',
+                '/(src="[^"]*)' . preg_quote(rawurlencode($pageid)) . '((?:\?[^#]*)?(?:#[^"]*)?")/',
                 '/(alt|title)="[^"]*"/'
             ),
             array(
-                "\${1}".rawurlencode($filename).'\2',
-                '\1="'.$imgtitle.'"'
+                "\${1}" . rawurlencode($filename) . '\2',
+                '\1="' . $imgtitle . '"'
             ),
             $imgtag
         );
@@ -292,7 +342,8 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
     /**
      * Exports configuration settings to $JSINFO
      */
-    function _exportToJSINFO(&$event) {
+    function _exportToJSINFO(&$event)
+    {
         global $JSINFO;
 
         $JSINFO['plugin_preservefilenames'] = array(
@@ -303,12 +354,16 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
     /**
      * Shows a list of media
      */
-    function _showMediaList(&$event) {
+    function _showMediaList(&$event)
+    {
         global $NS;
         global $AUTH;
         global $JUMPTO;
 
-        if ($event->data['do'] !== 'filelist') return;
+        if ($event->data['do'] !== 'filelist') {
+            return;
+        }
+
         $event->preventDefault();
 
         ptln('<div id="media__content">');
@@ -319,14 +374,17 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
     /**
      * Shows a list of media via ajax
      */
-    function _showMediaListAjax(&$event) {
+    function _showMediaListAjax(&$event)
+    {
         global $JUMPTO;
 
-        if ($event->data !== 'medialist_preservefilenames') return;
+        if ($event->data !== 'medialist_preservefilenames') {
+            return;
+        }
+
         $event->preventDefault();
 
-        require_once(DOKU_INC.'inc/media.php');
-
+        require_once(DOKU_INC . 'inc/media.php');
         $ns = cleanID($_POST['ns']);
         $auth = auth_quickaclcheck("$ns:*");
 
@@ -338,14 +396,15 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
      * 
      * @see media_filelist() in inc/media.php
      */
-    function _listMedia($ns, $auth, $jumpto) {
+    function _listMedia($ns, $auth, $jumpto)
+    {
         global $conf;
         global $lang;
 
-        print '<h1 id="media__ns">:'.hsc($ns).'</h1>'.NL;
+        print '<h1 id="media__ns">:' . hsc($ns) . '</h1>' . NL;
 
         if ($auth < AUTH_READ) {
-            print '<div class="nothing">'.$lang['nothingfound'].'</div>'.NL;
+            print '<div class="nothing">' . $lang['nothingfound'] . '</div>' . NL;
         } else {
             media_uploadform($ns, $auth);
 
@@ -355,22 +414,28 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
                     array('showmsg' => true, 'depth' => 1), $dir);
 
             if (empty($data)) {
-                print '<div class="nothing">'.$lang['nothingfound'].'</div>'.NL;
+                print '<div class="nothing">' . $lang['nothingfound'] . '</div>' . NL;
             } else {
                 foreach ($data as $item) {
                     $filename = $this->_getOriginalFileName($item['id']);
-                    if ($filename !== false) $item['file'] = utf8_encodeFN($filename);
+
+                    if ($filename !== false) {
+                        $item['file'] = utf8_encodeFN($filename);
+                    }
+
                     media_printfile($item, $auth, $jumpto);
                 }
             }
         }
+
         media_searchform($ns);
     }
 
     /**
      * Returns original filename if exists
      */
-    function _getOriginalFileName($id) {
+    function _getOriginalFileName($id)
+    {
         $meta = unserialize(io_readFile(metaFN($id, '.filename'), false));
         return empty($meta['filename']) ? false : $this->common->_sanitizeFileName($meta['filename']);
     }
@@ -382,7 +447,8 @@ class action_plugin_preservefilenames_anteater extends DokuWiki_Action_Plugin {
      *       filename of the snippet. For more details please see 
      *       common->_correctBasename().
      */
-    function _replaceSnippetDownload(&$event) {
+    function _replaceSnippetDownload(&$event)
+    {
         global $ACT;
 
         // $ACT is not clean, but in most cases this works fine
--- renderer.php
+++ renderer.php
@@ -7,34 +7,49 @@
  */
 
 // must be run within DokuWiki
-if (!defined('DOKU_INC')) die();
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
+if (!defined('DOKU_INC')) {
+    die();
+}
 
-require_once(DOKU_INC.'inc/parser/code.php');
-require_once(DOKU_PLUGIN.'preservefilenames/common.php');
+if (!defined('DOKU_PLUGIN')) {
+    define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
+}
 
-class renderer_plugin_preservefilenames extends Doku_Renderer_code {
+require_once(DOKU_INC . 'inc/parser/code.php');
+require_once(DOKU_PLUGIN . 'preservefilenames/common.php');
+
+class renderer_plugin_preservefilenames extends Doku_Renderer_code
+{
     /**
      * Outputs code snippet with a correct filename
      * 
      * @see Doku_Renderer_code::code
      */
-    function code($text, $language = NULL, $filename = '') {
+    function code($text, $language = NULL, $filename = '')
+    {
         // do nothing if codeblock number not matched
-        if ($_REQUEST['codeblock'] != $this->_codeblock++) return;
+        if ($_REQUEST['codeblock'] != $this->_codeblock++) {
+            return;
+        }
 
         $common = new PreserveFilenames_Common();
 
         $language = $common->_sanitizeFilename($language);
-        if (!$language) $language = 'txt';
+
+        if (!$language) {
+            $language = 'txt';
+        }
 
         $filename = $common->_correctBasename($filename);
         $filename = $common->_sanitizeFilename($filename);
-        if (!$filename) $filename = 'snippet.'.$language;
 
-        header("Content-Type: text/plain; charset=utf-8");
+        if (!$filename) {
+            $filename = 'snippet.' . $language;
+        }
+
+        header('Content-Type: text/plain; charset=utf-8');
         header($common->_buildContentDispositionHeader('download', $filename, 'no_pathinfo'));
-        header("X-Robots-Tag: noindex");
+        header('X-Robots-Tag: noindex');
         print trim($text, "\r\n");
         exit;
     }
